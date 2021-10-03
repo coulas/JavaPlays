4f4b9d0d124939a1271ecbb8b0c59ba55bb08e25
Nicolas Fédou <nicolas.fedou@gmail.com>
2021-10-03 23:09:20 +0200

    Refactoring towards spring like demo

--- src/test/java/io/permasoft/katas/javaplays/exceptions/SpringExceptionHandlingTest.java
+++ src/test/java/io/permasoft/katas/javaplays/exceptions/SpringExceptionHandlingTest.java
@@ -1,14 +1,52 @@
 package io.permasoft.katas.javaplays.exceptions;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.UnsatisfiedDependencyException;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
-@SpringBootTest
-@ActiveProfiles(profiles = "cycle")
-public class SpringExceptionHandlingTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SpringExceptionHandlingTest {
+    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner();
+    private Logger log = LoggerFactory.getLogger(SpringExceptionHandlingTest.class);
+
     @Test
-    void name() {
+    @DisplayName("raw springBootApplication works")
+    void without_added_profiles_it_just_works() {
         // made a cycle in test config to force a cascading exception in spring.
+        contextRunner
+                .withUserConfiguration(SpringSampleApplication.class)
+                .run(applicationContext -> {
+                            log.warn("this run hasn't failed as expected : "+ applicationContext.getStartupFailure());
+                            assertThat(applicationContext)
+                                    .hasNotFailed();
+                        }
+                );
+    }
+
+    @Test
+    @DisplayName("Cycle profile add a second bean for a bean definition without primary or qualifier available that make startup fails ")
+    void duplicateBeanWithoutSelectorsFailsStartup() {
+        contextRunner.withInitializer((context) ->
+                        context.getEnvironment().addActiveProfile("cycle"))
+                .withUserConfiguration(SpringSampleApplication.class)
+                .run(applicationContext -> {
+                            log.warn("this run failed as expected : ", applicationContext.getStartupFailure());
+                            assertThat(applicationContext)
+                                    .getFailure()
+                                    .isInstanceOf(UnsatisfiedDependencyException.class)
+                                    .hasMessageContainingAll(
+                                            "UnsatisfiedDependencyException",
+                                            "exceptionEndPoint",
+                                            "exceptionUseCases",
+                                            "exceptionStore",
+                                            "No qualifying bean of type", "exceptionDao", "expected single matching bean but found 2: exceptionDaoFailingImpl,exceptionDaoImpl")
+                                    .getRootCause().hasMessageContainingAll(
+                                            "No qualifying bean of type", "exceptionDao", "expected single matching bean but found 2: exceptionDaoFailingImpl,exceptionDaoImpl");
+                        }
+                );
     }
 }
